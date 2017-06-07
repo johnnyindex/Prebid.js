153b420dd427e24054ab11ee4adc6a457ab5ac8a
Matt Lane <mlane@appnexus.com>
2017-06-06 17:09:51 -0700

    Use command queue to process rendering

--- src/adapters/appnexusAst.js
+++ src/adapters/appnexusAst.js
@@ -280,10 +280,11 @@ function AppnexusAstAdapter() {
   }
 
   function onOutstreamRendererLoaded(bid) {
-    postMessage(JSON.stringify({
-      message: 'outstreamRendererLoaded',
-      bid
-    }), utils.getTopWindowUrl());
+    // not using this loaded property yet
+    bid.renderer.loaded = true;
+
+    // should this go in Renderer rather than be left to adapters to impelement?
+    bid.renderer.process();
   }
 
   function handleOutstreamRendererEvents(id, eventName) {
@@ -316,6 +317,7 @@ function AppnexusAstAdapter() {
             id: ad.renderer_id,
             url: ad.renderer_url,
             config: { adText: `AppNexus Outstream Video Ad via Prebid.js` },
+            loaded: false,
             callback: () => onOutstreamRendererLoaded.call(null, bid)
           });
 
